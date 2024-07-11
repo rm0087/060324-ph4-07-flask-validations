de5c3b9d4c0769e2dd08638b09007063b2063417
brewchetta <chett.tiller@gmail.com>
2024-07-11 14:41:32 -0400

    update config again

--- server/app.py
+++ server/app.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_cors import CORS
-import sqlalchemy
-
+from flask import request
+from config import app, db
 from models import db, User
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-CORS(app)
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
 
 @app.get('/')
 def index():
